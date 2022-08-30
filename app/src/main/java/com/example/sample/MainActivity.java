a8a123b9da8059754f03803116c62e569bed7123
MERCUROUS <sameershekhar100@gmail.com>
2022-08-31 02:58:02 +0530

    day 4

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.sample;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,31 +14,37 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.skydoves.powerspinner.PowerSpinnerView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText e1;
-    Button getOTP,Submit;
+    Button getOTP, Submit;
+    FirebaseAuth  auth = FirebaseAuth.getInstance();
     ProgressBar p;
-    Spinner dept,name;
+    Spinner dept, name;
     LinearLayout layout;
-    ArrayList<String> dept_array=new ArrayList<>();
-    ArrayList<String> name_array=new ArrayList<>();
-    String currDept,currName,email;
+    ArrayList<String> dept_array = new ArrayList<>();
+    ArrayList<String> name_array = new ArrayList<>();
+    String currDept, currName, email;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,18 +65,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //set Viewhandlers
-    private void getViews(){
-        e1=findViewById(R.id.OTP);
-        getOTP=findViewById(R.id.verify);
-        Submit=findViewById(R.id.submit);
-        dept=findViewById(R.id.department);
-        name=findViewById(R.id.name);
-        layout=findViewById(R.id.checklayout);
-        p=findViewById(R.id.progress);
+    private void getViews() {
+        e1 = findViewById(R.id.OTP);
+        getOTP = findViewById(R.id.verify);
+        Submit = findViewById(R.id.submit);
+        dept = findViewById(R.id.department);
+        name = findViewById(R.id.name);
+        layout = findViewById(R.id.checklayout);
+        p = findViewById(R.id.progress);
     }
 
     //find the dept
-    private void findDept(){
+    private void findDept() {
         dept_array.add("None");
         dept_array.add("Mechanical");
         dept_array.add("Electrical");
@@ -96,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                currDept=dept_array.get(0);
+                currDept = dept_array.get(0);
                 findName();
             }
 
@@ -107,30 +112,29 @@ public class MainActivity extends AppCompatActivity {
 
     //find name
     private void findName() {
-        if(name_array.size()>1)
-        {
+        if (name_array.size() > 1) {
 
             name_array.clear();
             name_array.add("None");
         }
-        if(currDept.equals("None")){
+        if (currDept.equals("None")) {
             Toast.makeText(this, "Select your Department", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        ref=db.collection("Department").document(currDept).collection("names");
+        ref = db.collection("Department").document(currDept).collection("names");
         p.setVisibility(View.VISIBLE);
         ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    Log.v("Taggg",task.getResult().size()+"");
+                    Log.v("Taggg", task.getResult().size() + "");
                     name_array.clear();
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        String name= document.getString("name");
+                        String name = document.getString("name");
                         name_array.add(name);
                     }
-                    Toast.makeText(MainActivity.this, ""+name_array.size(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "" + name_array.size(), Toast.LENGTH_SHORT).show();
                     setNameSpinner();
                     p.setVisibility(View.GONE);
                 }
@@ -139,52 +143,85 @@ public class MainActivity extends AppCompatActivity {
         // Log.v("Tag....",name_array.size()+"");
     }
 
-    private void setNameSpinner(){
+    private void setNameSpinner() {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, name_array);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         name.setAdapter(adapter);
         name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                currName=name_array.get(i);
-                Log.v("TAGGGGgg", ""+currName);
-                Log.v("TAGGGGgg",currDept+"");
+                currName = name_array.get(i);
+                Log.v("TAGGGGgg", "" + currName);
+                Log.v("TAGGGGgg", currDept + "");
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                currName="Select Your Name";
+                currName = "Select Your Name";
             }
 
         });
     }
 
     private void verification() {
-        if(currDept.isEmpty() || currDept.equals("None")) {
+        if (currDept.isEmpty() || currDept.equals("None")) {
             Toast.makeText(this, "Select your department", Toast.LENGTH_SHORT).show();
 
-        }
-        else if(currName==null || currName.equals("None")){
+        } else if (currName == null || currName.equals("None")) {
             Toast.makeText(this, "Select your name", Toast.LENGTH_SHORT).show();
         }
         // TODO Verify...
-        getEmail();
-        FirebaseAuth auth = FirebaseAuth.getInstance();
 
+        getEmail();//email=null
+       // while(email==null);
+    }
 
+    void getEmail() {
+      //  Toast.makeText(this, "dept:" + currDept + " name:" + currName , Toast.LENGTH_SHORT).show();
+        ref.document("arch1").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    email= documentSnapshot.getString("email");
+//                    Log.v("gandu mc",email+"");
+//                    System.out.println(email+"");
+                    signIn();
+                }
+            }
+        });
+    }
+//    public void onStart() {
+//        super.onStart();
+//        FirebaseUser currentUser = auth.getCurrentUser();
+//        if(currentUser != null){
+//            startActivity(new Intent(getApplicationContext(),ComplaintActivity.class));
+//        }
+//    }
+    void signIn(){
 
+//        auth.signInWithEmailAndPassword(email, "abc123").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    FirebaseUser user=auth.getCurrentUser();
+//                    startActivity(new Intent(getApplicationContext(),ComplaintActivity.class));
+//                    finish();
+//                }
+//            }
+//        });
         ActionCodeSettings actionCodeSettings =
                 ActionCodeSettings.newBuilder()
                         // URL you want to redirect back to. The domain (www.example.com) for this
                         // URL must be whitelisted in the Firebase Console.
+                        .setUrl("https://www.example.com/finishSignUp?cartId=1234")
                         // This must be true
                         .setHandleCodeInApp(true)
                         .setAndroidPackageName(
                                 "com.example.sample",
-                                true, /* installIfNotAvailable */
-                                "23"    /* minimumVersion */)
+                                false, /* installIfNotAvailable */
+                                null   /* minimumVersion */)
                         .build();
-        auth.sendSignInLinkToEmail(email,actionCodeSettings)
+        auth.sendSignInLinkToEmail(email, actionCodeSettings)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -193,22 +230,5 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-       Toast.makeText(this, "dept:"+currDept+" name:"+currName+" email:"+ email+"", Toast.LENGTH_SHORT).show();
-
-    }
-
-    void getEmail(){
-        ref.document("arch1").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        email = document.getString("email");
-
-                    }
-                }
-            }
-        });
     }
 }
