c0d005a243d26c34d2925c53854c304b6394de55
MERCUROUS <sameershekhar100@gmail.com>
2022-09-09 03:27:06 +0530

    day6

--- app/src/main/java/com/example/sample/Complain.java
+++ app/src/main/java/com/example/sample/Complain.java
@@ -1,4 +1,58 @@
 package com.example.sample;
 
 public class Complain {
+    String location,description,number;
+    int status;
+    String category;
+
+    //constructor
+    public Complain(String location, String description, String number, int status, String category) {
+        this.location = location;
+        this.description = description;
+        this.number = number;
+        this.status = status;
+        this.category = category;
+    }
+
+    //getters and setters
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 }
--- app/src/main/java/com/example/sample/ComplaintActivity.java
+++ app/src/main/java/com/example/sample/ComplaintActivity.java
@@ -5,17 +5,30 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
 public class ComplaintActivity extends AppCompatActivity {
 RecyclerView recyclerView;
 RecyclerView.LayoutManager layoutManager;
+ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
         getSupportActionBar().hide();
+
+        imageButton=findViewById(R.id.logout);
+        imageButton.setOnClickListener(view -> FirebaseAuth.getInstance().signOut());
+
+        setComplaintView();
+
+    }
+    void setComplaintView(){
         layoutManager=new LinearLayoutManager(getApplicationContext());
         recyclerView=findViewById(R.id.complain_list);
         recyclerView.setLayoutManager(layoutManager);
@@ -31,6 +44,5 @@ RecyclerView.LayoutManager layoutManager;
         complaint.add("test");
         ComplainAdaptar complainAdaptar=new ComplainAdaptar(complaint);
         recyclerView.setAdapter(complainAdaptar);
-
     }
 }
